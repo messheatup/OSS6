98c885bd0e78996afd9dfc1ce69146bdd8987b93
tamama9102 <tamama9102@naver.com>
2016-05-10 15:56:05 +0900

    Update hong.cpp

--- hong.cpp
+++ hong.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
+  int Jin =0;
   printf("Hellow World");
   printf("I want commit this!");
   
   for(i=0;i<100;i++)
   {
     puts("재진이 바보/n");
+    if(Jin == "0")
     puts("내가 제일 만만하니/n");
   }
 }
